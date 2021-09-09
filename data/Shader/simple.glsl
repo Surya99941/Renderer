be12b1ec70fa16b2d46b62cdd3f718a9e7c20d54
SuryaPrakash.S <48434643+Surya99941@users.noreply.github.com>
2021-09-09 14:56:21 +0530

    Add files via upload

---
+++ data/Shader/simple.glsl
@@ -0,0 +1,14 @@
+$vertex
+#version 330 core
+layout (location = 0) in vec3 aPos;
+void main(){
+   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
+}
+
+$fragment
+#version 330 core
+out vec4 FragColor;
+void main()
+{
+   FragColor = vec4(1.0f, 1.0f, 1.0f, 1.0f);
+}
